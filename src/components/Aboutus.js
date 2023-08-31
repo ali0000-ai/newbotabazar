346465377b689a2dd66cdff86ed1bfd3a3706ba3
ali0000-ai <143735394+ali0000-ai@users.noreply.github.com>
2023-08-31 21:08:57 +0500

    changed topic of about us

---
+++ src/components/Aboutus.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Aboutus() {
+  return (
+    <div>
+      about us page
+      <div className="text-lg">hello this is about us</div>
+    </div>
+  );
+}
